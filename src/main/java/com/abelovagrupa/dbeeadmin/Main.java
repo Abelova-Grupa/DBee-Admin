8c10934572de90df129a4109ce306ae4079a3902
jelisavac-l <jelisavacluka63@gmail.com>
2025-02-17 01:51:50 +0100

    test(editor): Testing syntax highlighting.

--- src/main/java/com/abelovagrupa/dbeeadmin/Main.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/Main.java
@@ -12,10 +12,14 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
         stage.setTitle("DBee Admin 1.0-SNAPSHOT");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
+
     }
 
     public static void main(String[] args) {
