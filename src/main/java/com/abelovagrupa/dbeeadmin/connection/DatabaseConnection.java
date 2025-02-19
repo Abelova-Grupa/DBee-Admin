fde6b973783e71ffe85f089276b34aae47d464ce
jelisavac-l <jelisavacluka63@gmail.com>
2025-02-19 10:56:59 +0100

    chore(Connection): Minor reformatting.

--- src/main/java/com/abelovagrupa/dbeeadmin/connection/DatabaseConnection.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/connection/DatabaseConnection.java
@@ -10,19 +10,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+
+    // TODO: Split url to host & port
+
     private static DatabaseConnection instance;
-    private Connection connection;
+    private final Connection connection;
 
     private DatabaseConnection() {
+
+        // Load data from .env file
         Dotenv dotenv = Dotenv.load();
         String dbUrl = dotenv.get("DB_URL");
         String dbUsername = dotenv.get("DB_USERNAME");
         String dbPassword = dotenv.get("DB_PASSWORD");
 
         try {
+            assert dbUrl != null;
+            assert dbUsername != null;
             connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-            System.out.println("Database connection established");
-        } catch (SQLException e) {
+            System.out.println("Database connection established.");
+        } catch (SQLException | AssertionError e) {
             throw new RuntimeException(e);
         }
     }
@@ -33,27 +40,37 @@ public class DatabaseConnection {
         }
         return instance;
     }
+
     public Connection getConnection() {
         return connection;
     }
+
     public void closeConnection() {
         try {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
-                System.out.println("Database connection closed");
+                System.out.println("Database connection closed.");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public void writtingInEnvFile(String dbUrl, String dbUsername, String dbPassword) throws IOException {
+
+    /**
+     * Saves connection parameters to env file.
+     * @param dbUrl full database url
+     * @param dbUsername user with full privileges
+     * @param dbPassword password
+     * @throws IOException on .env writing failure
+     */
+    public void setConnectionParameters(String dbUrl, String dbUsername, String dbPassword) throws IOException {
         String filePath = ".env";
         System.out.println("Database connection established");
         System.out.println("Writing to file: " + filePath);
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(filePath, false));
-        fileWriter.write("DB_URL="+dbUrl + "\n");
-        fileWriter.write("DB_USERNAME="+dbUsername + "\n");
-        fileWriter.write("DB_PASSWORD="+dbPassword + "\n");
+        fileWriter.write("DB_URL=" + dbUrl + "\n");
+        fileWriter.write("DB_USERNAME=" + dbUsername + "\n");
+        fileWriter.write("DB_PASSWORD=" + dbPassword + "\n");
         fileWriter.close();
     }
 
