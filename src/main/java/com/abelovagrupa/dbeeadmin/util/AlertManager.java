9fc2b855e06e694aedde43840eed8b90914687f2
jelisavac-l <jelisavacluka63@gmail.com>
2025-02-20 06:20:14 +0100

    feat(alerts): Alert manager.

---
+++ src/main/java/com/abelovagrupa/dbeeadmin/util/AlertManager.java
@@ -0,0 +1,39 @@
+package com.abelovagrupa.dbeeadmin.util;
+
+import javafx.scene.control.Alert;
+
+public class AlertManager {
+
+    // This approach was employed to prevent dependency hell. And to make it look nice.
+
+    public static void showConfirmationDialog(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        display(alert, title, header, content);
+    }
+
+    public static void showInformationDialog(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        display(alert, title, header, content);
+    }
+
+    public static void showWarningDialog(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        display(alert, title, header, content);
+    }
+
+    public static void showErrorDialog(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        display(alert, title, header, content);
+    }
+
+    private static void display(Alert alert, String title, String header, String content) {
+        assert content != null;
+
+        alert.setTitle((title == null) ? "DBee Admin" : title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+
+        alert.showAndWait();
+    }
+
+}
