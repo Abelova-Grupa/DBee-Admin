91f3035afe99a397dced8d669caafcb675c7ecc6
jelisavac-l <jelisavacluka63@gmail.com>
2025-03-11 21:36:40 +0100

    fix: SQL Preview syntax highlighting and styles.

--- src/main/java/com/abelovagrupa/dbeeadmin/view/DialogSQLPreview.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/DialogSQLPreview.java
@@ -4,18 +4,26 @@ import com.abelovagrupa.dbeeadmin.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class DialogSQLPreview extends Dialog<String> {
+public class DialogSQLPreview extends Dialog<Boolean> {
     @FXML
     ScrollPane scrollContent;
 
     @FXML
-    Label lblTitle;
+    CodeArea codeArea;
 
     @FXML
-    TextArea taEditor;
+    Label lblTitle;
 
     public DialogSQLPreview(String sql) {
         // Load FXML and initialize the dialog
@@ -46,19 +54,58 @@ public class DialogSQLPreview extends Dialog<String> {
         // Set up the result converter (this determines what the dialog returns)
         this.setResultConverter(buttonType -> {
             if (buttonType == ButtonType.APPLY) {
-                return "SQL Preview Finished";
+                return true;
             }
             return null;
         });
 
+        // Setup the code area
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+        codeArea.textProperty().addListener((_, _, newText) -> {
+            codeArea.setStyleSpans(0, computeHighlighting(newText));
+        });
+
+
         this.setTitle("SQL Preview");
         this.setSqlPreviewContent(sql);
         this.getDialogPane().setMinWidth(500);
+        this.getDialogPane().getScene().getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
 
     }
 
     public void setSqlPreviewContent(String sqlContent) {
-        taEditor.setText(sqlContent);
+        codeArea.appendText(sqlContent);
+    }
+
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", PanelScript.SQL_KEYWORDS) + ")\\b";
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    private static final String NUMBER_PATTERN = "\\b\\d+\\b";
+    private static final String COMMENT_PATTERN = "--[^\n]*" + "|" + "/\\*[^\\*]*\\*/";
+
+    private static final Pattern PATTERN = Pattern.compile(
+        "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+            + "|(?<STRING>" + STRING_PATTERN + ")"
+            + "|(?<NUMBER>" + NUMBER_PATTERN + ")"
+            + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text.toUpperCase());
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+        while (matcher.find()) {
+            String styleClass =
+                matcher.group("KEYWORD") != null ? "keyword" :
+                    matcher.group("STRING") != null ? "string" :
+                        matcher.group("NUMBER") != null ? "number" :
+                            matcher.group("COMMENT") != null ? "comment" :
+                                null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
     }
 
 }
