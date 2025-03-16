2335e17ef447335f568f3f237660059728828142
jelisavac-l <jelisavacluka63@gmail.com>
2025-03-16 23:40:53 +0100

    refactor: Fixed incorrect naming and structure regarding ProgramState class.

--- src/main/java/com/abelovagrupa/dbeeadmin/services/ProgramState.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/services/ProgramState.java
@@ -1,25 +1,52 @@
 package com.abelovagrupa.dbeeadmin.services;
 
 import com.abelovagrupa.dbeeadmin.model.schema.Schema;
+import com.abelovagrupa.dbeeadmin.model.table.Table;
 
 public class ProgramState {
 
-    private static ProgramState instance;
-    private Schema currentSchema;
+    private static ProgramState instance = null;
 
-    private ProgramState() {}
+    private Schema selectedSchema;
+    private Table selectedTable;
+
+    private ProgramState() {
+        System.out.println("Initializing program state.");
+    }
 
     public static ProgramState getInstance() {
-        if(instance == null) instance = new ProgramState();
+        if(instance==null) {
+            instance = new ProgramState();
+        }
         return instance;
     }
 
-    public Schema getCurrentSchema() {
-        return currentSchema;
+    public void clearSelection() {
+        this.selectedSchema = null;
+        this.selectedTable = null;
     }
 
-    public void setCurrentSchema(Schema currentSchema) {
-        this.currentSchema = currentSchema;
+    public Schema getSelectedSchema() {
+        return selectedSchema;
     }
 
+    public void setSelectedSchema(Schema selectedSchema) {
+        this.selectedSchema = selectedSchema;
+    }
+
+    public Table getSelectedTable() {
+        return selectedTable;
+    }
+
+    public void setSelectedTable(Table selectedTable) {
+        this.selectedTable = selectedTable;
+    }
+
+    @Override
+    public String toString() {
+        return "DatabaseAudit{" +
+            "selectedSchema=" + selectedSchema +
+            ", selectedTable=" + selectedTable +
+            '}';
+    }
 }
