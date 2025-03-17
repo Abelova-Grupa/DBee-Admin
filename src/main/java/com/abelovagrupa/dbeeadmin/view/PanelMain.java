eda68755c6d247632eced23fe7e8a303bf77f9c9
LukaDervisevic <lukadervisevic@gmail.com>
2025-03-17 21:38:54 +0100

    ui(newSchema): Moving newSchema dialog to a tab

--- src/main/java/com/abelovagrupa/dbeeadmin/view/PanelMain.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/PanelMain.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Screen;
@@ -205,57 +206,61 @@ public class PanelMain implements Initializable {
     public void newTableTab() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("panelTableCreation.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-
-        stage.setTitle("DBee Admin - New Table");
-        stage.setScene(scene);
-
-        stage.setOnShown(event -> {
-            // Get the screen's bounds (width and height)
-            double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
-            double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
-
-            // Get the stage width and height
-            double stageWidth = stage.getWidth();
-            double stageHeight = stage.getHeight();
-
-            // Calculate the center position
-            stage.setX((screenWidth - stageWidth) / 2);
-            stage.setY((screenHeight - stageHeight) / 2);
-        });
-        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
-
-        stage.show();
+//        Scene scene = new Scene(fxmlLoader.load());
+//        Stage stage = new Stage();
+//        stage.initModality(Modality.APPLICATION_MODAL);
+//
+//        stage.setTitle("DBee Admin - New Table");
+//        stage.setScene(scene);
+//
+//        stage.setOnShown(event -> {
+//            // Get the screen's bounds (width and height)
+//            double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+//            double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+//
+//            // Get the stage width and height
+//            double stageWidth = stage.getWidth();
+//            double stageHeight = stage.getHeight();
+//
+//            // Calculate the center position
+//            stage.setX((screenWidth - stageWidth) / 2);
+//            stage.setY((screenHeight - stageHeight) / 2);
+//        });
+//        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
+//
+//        stage.show();
     }
 
     public void newSchemaTab(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("panelSchemaCreation.fxml"));
+        Tab schemaTab = fxmlLoader.load();
+        editorController.editorTabs.getTabs().add(schemaTab);
+        editorController.editorTabs.getSelectionModel().select(schemaTab);
 
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
 
-        stage.setTitle("DBee Admin - New Schema");
-        stage.setScene(scene);
-
-        stage.setOnShown(event -> {
-            // Get the screen's bounds (width and height)
-            double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
-            double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
-
-            // Get the stage width and height
-            double stageWidth = stage.getWidth();
-            double stageHeight = stage.getHeight();
-
-            // Calculate the center position
-            stage.setX((screenWidth - stageWidth) / 2);
-            stage.setY((screenHeight - stageHeight) / 2);
-        });
-        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
-
-        stage.show();
+//        Scene scene = new Scene(fxmlLoader.load());
+//        Stage stage = new Stage();
+//        stage.initModality(Modality.APPLICATION_MODAL);
+//
+//        stage.setTitle("DBee Admin - New Schema");
+//        stage.setScene(scene);
+//
+//        stage.setOnShown(event -> {
+//            // Get the screen's bounds (width and height)
+//            double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+//            double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+//
+//            // Get the stage width and height
+//            double stageWidth = stage.getWidth();
+//            double stageHeight = stage.getHeight();
+//
+//            // Calculate the center position
+//            stage.setX((screenWidth - stageWidth) / 2);
+//            stage.setY((screenHeight - stageHeight) / 2);
+//        });
+//        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
+//
+//        stage.show();
     }
 
     public void openConnectionSettings(ActionEvent actionEvent) throws IOException {
