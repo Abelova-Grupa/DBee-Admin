449971d03207f3c08debb7f8f3567d188463f2b4
LukaDervisevic <lukadervisevic@gmail.com>
2025-03-18 14:41:03 +0100

    feat(schemaCreation): #Implementing ui logic for schema creation

--- src/main/java/com/abelovagrupa/dbeeadmin/view/PanelSchemaCreation.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/PanelSchemaCreation.java
@@ -1,24 +1,22 @@
 package com.abelovagrupa.dbeeadmin.view;
 
-import com.abelovagrupa.dbeeadmin.model.column.Column;
-import com.abelovagrupa.dbeeadmin.model.column.DataType;
+import com.abelovagrupa.dbeeadmin.controller.DatabaseInspector;
 import com.abelovagrupa.dbeeadmin.model.schema.Charset;
 import com.abelovagrupa.dbeeadmin.model.schema.Collation;
+
 import com.abelovagrupa.dbeeadmin.model.schema.Schema;
-import com.abelovagrupa.dbeeadmin.model.table.Table;
 import com.abelovagrupa.dbeeadmin.services.DDLGenerator;
+import com.abelovagrupa.dbeeadmin.util.AlertManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class PanelSchemaCreation implements Initializable {
@@ -38,6 +36,10 @@ public class PanelSchemaCreation implements Initializable {
     @FXML
     public ImageView createDBImage;
 
+    @FXML
+    public TextField schemaNameTxtField;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<Charset> charsetObservableList = FXCollections.observableArrayList(Charset.values());
@@ -48,7 +50,46 @@ public class PanelSchemaCreation implements Initializable {
         createDBImage.setImage(new Image(getClass().getResource("/com/abelovagrupa/dbeeadmin/images/create-database.png").toExternalForm()));
     }
 
+    @FXML
     private void generate() {
+        if(schemaNameTxtField.getText().isBlank()){
+            AlertManager.showErrorDialog("Error","Schema name cannot be empty",null);
+            return;
+        }
+
+        if(schemaNameTxtField.getText().contains(" ")) {
+            AlertManager.showErrorDialog("Error","Schema name cannot have a blank character",null);
+            return;
+        }
+        if(Character.isDigit(schemaNameTxtField.getText().charAt(0))) {
+            AlertManager.showErrorDialog("Error","Schema name cannot have digits at the beginning",null);
+            return;
+        }
+        if(schemaNameTxtField.getText().matches(".*[^a-zA-Z0-9_].*")){
+            AlertManager.showErrorDialog("Error","Schema name cannot have special characters",null);
+            return;
+        }
+
+        if(cbCharset.getSelectionModel().getSelectedItem() == null){
+            AlertManager.showErrorDialog("Error","Charset is not selected",null);
+            return;
+        }
+
+        if(cbCollation.getSelectionModel().getSelectedItem() == null){
+            AlertManager.showErrorDialog("Error","Collation is not selected",null);
+            return;
+        }
+
+        String schemaName = schemaNameTxtField.getText();
+        Charset charset = cbCharset.getSelectionModel().getSelectedItem();
+        Collation collation = cbCollation.getSelectionModel().getSelectedItem();
+
+        try {
+            DDLGenerator.createDatabase(new Schema(schemaName,charset,collation,null,0,0L),true);
+            AlertManager.showConfirmationDialog("Success","Schema created",null);
+        } catch (SQLException e) {
+            AlertManager.showErrorDialog("Error","Exception while creating schema",e.toString());
+        }
 
     }
 }
