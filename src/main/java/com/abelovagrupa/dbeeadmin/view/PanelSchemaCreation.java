0213442d647dbfa826a863a4aa651fd0b23b13d6
LukaDervisevic <lukadervisevic@gmail.com>
2025-03-18 18:19:44 +0100

    fix(collaton): #Saving curent progress on collation combobox separation

--- src/main/java/com/abelovagrupa/dbeeadmin/view/PanelSchemaCreation.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/PanelSchemaCreation.java
@@ -54,15 +54,81 @@ public class PanelSchemaCreation implements Initializable {
             public void changed(ObservableValue<? extends Charset> observableValue, Charset oldValue, Charset newValue) {
                 if(newValue != oldValue){
                     cbCollation.setDisable(false);
+                    cbCollation.getItems().clear();
                     switch (newValue){
                         case DEFAULT -> {
-                            cbCollation.getItems().clear();
                             cbCollation.getItems().add(Collation.DEFAULT);
                         }
                         case ARMSCII8 -> {
-                            cbCollation.getItems().clear();
-                            cbCollation.getItems().addAll(Collation.ARMSCII8_BIN,Collation.ARMSCII8_GENERAL_CI);
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.ARMSCII8_BIN,Collation.ARMSCII8_GENERAL_CI);
                         }
+                        case ASCII -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.ASCII_BIN,Collation.ASCII_GENERAL_CI);
+                        }
+                        case BIG5 -> {
+                            cbCollation.getItems().clear();
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.BIG5_BIN,Collation.BIG5_CHINESE_CI);
+                        }
+                        case BINARY -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.BINARY);
+                        }
+                        case CP1250 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,
+                                    Collation.CP1250_BIN,
+                                    Collation.CP1250_CROATIAN_CI,
+                                    Collation.CP1250_CZECH_CS,
+                                    Collation.CP1250_GENERAL_CI);
+                        }
+                        case CP1251 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,
+                                    Collation.CP1251_BIN,
+                                    Collation.CP1251_BULGARIAN_CI,Collation.CP1251_GENERAL_CI,
+                                    Collation.CP1251_GENERAL_CS, Collation.CP1251_UKRAINIAN_CI);
+                        }
+                        case CP1256 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.CP1256_BIN,Collation.CP1256_GENERAL_CI);
+                        }
+                        case CP1257 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT, Collation.CP1257_BIN,Collation.CP1257_GENERAL_CI,Collation.CP1257_LITHUANIAN_CI);
+                        }
+                        case CP850 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.CP850_BIN,Collation.CP850_GENERAL_CI);
+                        }
+                        case CP852 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.CP852_BIN,Collation.CP852_GENERAL_CI);
+                        }
+                        case CP866 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.CP866_BIN,Collation.CP866_GENERAL_CI);
+                        }
+                        case CP932 -> {
+                            cbCollation.getItems().clear();
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.CP932_BIN,Collation.CP932_JAPANESE_CI);
+                        }
+                        case DEC8 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.DEC8_BIN,Collation.DEC8_SWEDISH_CI);
+                        }
+                        case EUCJPMS -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.EUCJPMS_BIN,Collation.EUCJPMS_JAPANESE_CI);
+                        }
+                        case EUCKR -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.EUCKR_BIN,Collation.EUCKR_KOREAN_CI);
+                        }
+                        case GB18030 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.GB18030_BIN,Collation.GB18030_CHINESE_CI);
+                        }
+                        case GB2312 -> {
+                            cbCollation.getItems().addAll(Collation.DEFAULT,Collation.GB2312_BIN,Collation.GB2312_CHINESE_CI);
+                        }
+                        case GBK -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.GBK_BIN,Collation.GBK_CHINESE_CI);
+                        case GEOSTD8 -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.GEOSTD8_BIN,Collation.GEOSTD8_GENERAL_CI);
+                        case GREEK -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.GREEK_BIN,Collation.GREEK_GENERAL_CI);
+                        case HEBREW -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.HEBREW_BIN,Collation.HEBREW_GENERAL_CI);
+                        case HP8 -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.HP8_BIN,Collation.HP8_ENGLISH_CI);
+                        case KEYBCS2 -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.KEYBCS2_BIN,Collation.KEYBCS2_GENERAL_CI);
+                        case KOI8R ->  cbCollation.getItems().addAll(Collation.DEFAULT,Collation.KOI8R_BIN,Collation.KOI8R_GENERAL_CI);
+                        case KOI8U -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.KOI8U_BIN,Collation.KOI8U_GENERAL_CI);
+                        case LATIN1 -> cbCollation.getItems().addAll(Collation.DEFAULT,Collation.LATIN1_BIN,Collation.LATIN1_GENERAL_CI,Collation.LATIN1_GENERAL_CS,Collation.LATIN1_GERMAN1_CI,Collation.LATIN1_GERMAN2_CI,Collation.LATIN1_DANISH_CI,Collation.LATIN1_SPANISH_CI,Collation.LATIN1_SWEDISH_CI);
+
                     }
                 }
             }
