990486435d28e73ba4c91663adb00d4e88d1ac26
LukaDervisevic <lukadervisevic@gmail.com>
2025-03-22 10:36:14 +0100

    chore(controllerStructure): #Creating initial controller strucuture for table creation tabs

--- src/main/java/com/abelovagrupa/dbeeadmin/view/PanelColumnTab.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/PanelColumnTab.java
@@ -16,59 +16,7 @@ import org.kordamp.ikonli.javafx.FontIcon;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PanelColumnTab implements Initializable {
+public class PanelColumnTab {
 
-    public TextField txtName;
-    public ComboBox<DataType> cbDataTypes;
-    public TextField txtSize;
-    public CheckBox cbZerofill;
-    public CheckBox cbPrimary;
-    public CheckBox cbNotNull;
-    public CheckBox cbAutoIncrement;
-    public CheckBox cbUnique;
-    public TextField txtDefault;
-    public TextField txtComment;
-    public Button btnDelete;
-    public HBox columnContainer;
 
-    private boolean deleted = false;
-
-    VBox parent;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<DataType> datatypes = FXCollections.observableArrayList(DataType.values());
-        cbDataTypes.setItems(datatypes);
-    }
-
-    public Column getColumn(Table table) {
-
-        if(this.deleted) return null;
-
-        return new Column.ColumnBuilder(txtName.getText(), cbDataTypes.getValue(), table)
-            .setPrimaryKey(cbPrimary.isSelected())
-            .setSize(txtSize.getText().isEmpty() ? null : Integer.parseInt(txtSize.getText()))
-            .setZeroFill(cbZerofill.isSelected())
-            .setNotNull(cbNotNull.isSelected())
-            .setAutoIncrement(cbAutoIncrement.isSelected())
-            .setUnique(cbUnique.isSelected())
-            .setDefaultValue(txtDefault.getText().isEmpty() ? null : txtDefault.getText())
-            .setComment(txtComment.getText().isEmpty() ? null : txtComment.getText())
-            .build();
-    }
-
-    // Maybe there is a way to extract a superclass, will worry about optimization later.
-    public void setParent(VBox parent)
-    {
-        this.parent = parent;
-    }
-
-    public Boolean isDeleted() {
-        return deleted;
-    }
-
-    public void handleDelete(ActionEvent actionEvent) {
-        parent.getChildren().remove(columnContainer);
-        this.deleted = true;
-    }
 }
--- src/main/java/com/abelovagrupa/dbeeadmin/view/PanelTableCreation.java
+++ src/main/java/com/abelovagrupa/dbeeadmin/view/PanelTableCreation.java
@@ -12,9 +12,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -40,45 +38,73 @@ public class PanelTableCreation implements Initializable {
     @FXML
     TextField txtTableName;
 
+    @FXML
+    TabPane tableAttributeTabPane;
+
     List<PanelColumnTab> columnControllers;
 
-    public void addColumn() throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("panelColumnTab.fxml"));
-        HBox column = loader.load();
-        PanelColumnTab controller = loader.getController();
-        controller.setParent(scrollContent);
-        columnControllers.add(controller);
-        scrollContent.getChildren().add(column);
-    }
 
-    public void createTable() {
+//    public void addColumn() throws IOException {
+//        FXMLLoader loader = new FXMLLoader(Main.class.getResource("panelColumnTab.fxml"));
+//        HBox column = loader.load();
+//        PanelColumnTab controller = loader.getController();
+//        controller.setParent(scrollContent);
+//        columnControllers.add(controller);
+//        scrollContent.getChildren().add(column);
+//    }
 
-        if(txtTableName.getText().isEmpty())
-        {
-            AlertManager.showErrorDialog(null, null, "Table name must not be empty.");
-            return;
-        }
-
-        // TODO: Import schema from programstate instead of this.
-        Schema tempSchema = new Schema.SchemaBuilder(txtTableName.getText().split("\\.")[0], null, null).build();
-        Table tempTable = new Table.TableBuilder(null, txtTableName.getText().split("\\.")[1], tempSchema, null).build();
-        List<Column> columns = new LinkedList<>();
-        for (PanelColumnTab c : columnControllers) {
-            if(c.isDeleted()) continue;
-            columns.add(c.getColumn(tempTable));
-        }
-        tempTable.setColumns(columns);
-        try {
-            DDLGenerator.createTable(tempTable, true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public void createTable() {
+//
+//        if(txtTableName.getText().isEmpty())
+//        {
+//            AlertManager.showErrorDialog(null, null, "Table name must not be empty.");
+//            return;
+//        }
+//
+//        // TODO: Import schema from programstate instead of this.
+//        Schema tempSchema = new Schema.SchemaBuilder(txtTableName.getText().split("\\.")[0], null, null).build();
+//        Table tempTable = new Table.TableBuilder(null, txtTableName.getText().split("\\.")[1], tempSchema, null).build();
+//        List<Column> columns = new LinkedList<>();
+//        for (PanelColumnTab c : columnControllers) {
+//            if(c.isDeleted()) continue;
+//            columns.add(c.getColumn(tempTable));
+//        }
+//        tempTable.setColumns(columns);
+//        try {
+//            DDLGenerator.createTable(tempTable, true);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Initialize column controllers
-        columnControllers = new ArrayList<>();
+        try {
+            // Initialize column tab
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("panelColumnTab.fxml"));
+            Tab columnsTab = loader.load();
+            tableAttributeTabPane.getTabs().add(columnsTab);
+            tableAttributeTabPane.getSelectionModel().select(columnsTab);
+
+            // Initialize index tab
+            loader = new FXMLLoader(Main.class.getResource("panelIndexTab.fxml"));
+            Tab indexTab = loader.load();
+            tableAttributeTabPane.getTabs().add(indexTab);
+
+            // Initialize foreign key tab
+            loader = new FXMLLoader(Main.class.getResource("panelFKTab.fxml"));
+            Tab foreignKeyTab = loader.load();
+            tableAttributeTabPane.getTabs().add(foreignKeyTab);
+
+            // Initialize trigger tab
+            loader = new FXMLLoader(Main.class.getResource("panelTriggerTab.fxml"));
+            Tab triggerTab = loader.load();
+            tableAttributeTabPane.getTabs().add(triggerTab);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         ObservableList<DBEngine> engines = FXCollections.observableArrayList(DBEngine.values());
         cbEngines.setItems(engines);
